6bae42b365737c86f7ae6e982f02093aedc18721
鎌田由美子 <feg.yu1892@gmail.com>
2026-03-17 10:19:21 +0900

    フラッシュメッセージ表示を実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class(message_type)
+    case message_type.to_sym
+    when :notice
+      "alert alert-success"
+    when :alert
+      "alert alert-error"
+    else
+      "alert alert-info"
+    end
+  end
 end
