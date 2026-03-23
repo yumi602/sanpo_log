cf8614d43a03d4c2168b2ef07137c72246291362
鎌田由美子 <feg.yu1892@gmail.com>
2026-03-23 13:19:29 +0900

    mood表示の見た目を修正

--- app/models/walk_record.rb
+++ app/models/walk_record.rb
@@ -3,6 +3,21 @@ class WalkRecord < ApplicationRecord
 
   enum mood: { good: 0, normal: 1, bad: 2, excited: 3, tired: 4 }
 
+  def mood_label
+    case mood
+    when "good"
+      "良い"
+    when "normal"
+      "ふつう"
+    when "bad"
+      "悪い"
+    when "excited"
+      "楽しい"
+    when "tired"
+      "疲れた"
+    end
+  end
+
   validates :walked_on, presence: true
   validates :body, presence: true
 end
