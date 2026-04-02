73e4441776853a35d0a9a35d1520db4391e5b33f
鎌田由美子 <feg.yu1892@gmail.com>
2026-04-02 10:59:23 +0900

    WalkRecord作成時の進捗更新処理の実装

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -24,10 +24,14 @@ class ChallengesController < ApplicationController
   end
 
   def show
-    @challenge = current_user.challenges.find_by(status: :in_progress)
+    @challenge = current_user.challenges
+                            .where(status: [:in_progress, :completed])
+                            .order(created_at: :desc)
+                            .first
+
     if @challenge.nil?
-      redirect_to new_challenge_path, alert: "進行中のチャレンジがありません"
-      nil
+      redirect_to new_challenge_path, alert: "チャレンジがありません"
+      return
     end
   end
 end
--- app/controllers/walk_records_controller.rb
+++ app/controllers/walk_records_controller.rb
@@ -14,6 +14,7 @@ class WalkRecordsController < ApplicationController
     @walk_record = current_user.walk_records.build(walk_record_params)
 
     if @walk_record.save
+      update_challenge_progress
       redirect_to walk_records_path, notice: "散歩記録を投稿しました"
     else
       flash.now[:alert] = "散歩記録を投稿できませんでした"
@@ -51,5 +52,21 @@ class WalkRecordsController < ApplicationController
 
   def walk_record_params
     params.require(:walk_record).permit(:title, :body, :walked_on, :mood)
+  end
+  def update_challenge_progress
+    current_challenge = current_user.challenges.find_by(status: :in_progress)
+    return if current_challenge.nil?
+
+    same_day_records_count = current_user.walk_records.where(walked_on: @walk_record.walked_on).count
+    return unless same_day_records_count == 1
+
+    current_challenge.progress_count += 1
+
+    if current_challenge.progress_count >= 7
+      current_challenge.status = :completed
+      current_challenge.completed_at = Time.current
+    end
+
+    current_challenge.save!
   end
 end
